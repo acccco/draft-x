6a9155286db2d3481e96262016518a4f69605908
aco <wwsxuan@163.com>
2019-02-19 16:26:41 +0800

    path bug

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -2,7 +2,7 @@
  * @Author: Aco
  * @Date: 2018-11-23 09:27:56
  * @LastEditors: Aco
- * @LastEditTime: 2019-02-19 16:24:33
+ * @LastEditTime: 2019-02-19 16:26:04
  * @Description: file content
  */
 
@@ -12,8 +12,8 @@ export * from './blockData';
 export * from './blockIndent';
 export * from './blockType';
 export * from './control';
-export * from './inlineStyleaa';
-export * from './inlineTagaa';
+export * from './inlineStyle';
+export * from './inlineTag';
 export * from './inlineBlock';
 export * from './decorator';
 export * from './atomic';
