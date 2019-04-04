93ebd45fd7f7b10c3e8ceedcb8b27bea72d6e9db
aco <wwsxuan@163.com>
2019-04-04 13:21:54 +0800

    update

--- example/richEditor/src/plugin/Test.jsx
+++ example/richEditor/src/plugin/Test.jsx
@@ -2,17 +2,13 @@
  * @Author: Aco
  * @Date: 2018-11-05 09:26:47
  * @LastEditors: Aco
- * @LastEditTime: 2019-02-20 10:10:17
+ * @LastEditTime: 2019-04-04 13:10:19
  * @Description: 测试专用
  */
 
 import { BasePlugin } from 'draft-x';
 
 export default class BaseAtomicase extends BasePlugin {
-  constructor() {
-    super();
-  }
-
   toggle(data) {
     this.fire(editorState => {
       return editorState;
